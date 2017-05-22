1b0a4a229e56315376128d5d9e8971cfaa4fcb13
Rawane ZOSSOU <rzossous@exchange.unowhy.com>
2017-05-22 14:13:52 +0200

    embed the app into #app element

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -4,11 +4,14 @@ import Vue from "vue"
 import VueRouter from 'vue-router'
 import router from './router'
 
+import App from './components/app'
 import Domain from './components/domain'
 import DomainList from './components/domain-list'
 import MenuButton from './components/menu-button'
 import Task from './components/task'
 import Timer from './components/timer'
+
+Vue.use(VueRouter)
 
 Vue.component('domain', Domain)
 Vue.component('domain-list', DomainList)
@@ -16,14 +19,8 @@ Vue.component('menu-button', MenuButton)
 Vue.component('task', Task)
 Vue.component('timer', Timer)
 
-Vue.use(VueRouter)
-var RouterLink = Vue.component('router-link')
-
 new Vue({
-  components: {
-    RouterLink,
-  },
   router: router,
   el: '#app',
-  render: h => h(Vue.component('router-view'))
+  render: h => h(App)
 })
--- web/static/js/components/app.vue
+++ web/static/js/components/app.vue
@@ -1,21 +1,10 @@
 <template>
   <div id="app">
-    <domain :domain="domain"></domain>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
   export default {
-    data: function() {
-      return {
-        domain: {
-          name: "hobby",
-          tasks: [
-            {id: 1, comment: "release planr", archived: false},
-            {id: 2, comment: "celebrate!", archived: false}
-          ]
-        }
-      }
-    }
   }
 </script>
